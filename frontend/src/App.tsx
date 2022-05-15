8e8eb7c06e4c3366067989473112e48833657b57
LorenzzoCasale <lorenzzocasale@gmail.com>
2022-05-15 06:08:57 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from 'components/Navbar';
+import { ReactComponent as GithubIcon } from './assets/img/github.svg'
 
 function App() {
   return (
-
-    <h1>Página principal</h1>
-
+    <Navbar />
   );
+
 }
 
 export default App;
